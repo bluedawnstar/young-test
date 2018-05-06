b997a0cdfe13022fadd8cc90fccc8ce7f991b379
youngman <youngman.ro@gmail.com>
2018-05-07 00:13:24 +0900

    [Add] visual studio project

--- test/test.cpp
+++ test/test.cpp
@@ -4,27 +4,20 @@
 
 using namespace std;
 
-bool gTestA = false;
-bool gTestB = false;
-bool gTestC = false;
-
 bool testB() {
     cout << "TEST B" << endl;
-    gTestB = true;
     return true;
 }
 
 struct TestC {
     bool operator()() const {
         cout << "TEST C" << endl;
-        gTestC = true;
         return true;
     }
 };
 
 YTEST_ADD_CASE("a", []() {
     cout << "TEST A" << endl;
-    gTestA = true;
     return true;
 });
 
